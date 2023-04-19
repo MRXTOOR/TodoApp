89937722348a548fcdc94b2953f7b207429f950f
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-19 18:22:37 +0300

    verbose

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-
 class Todo(models.Model):
-    title = models.CharField()
+    title = models.CharField("Название задания", max_length=550)
+    is_complete = models.BooleanField("Задание завершено",default=False)
+    
+
+    def __str__(self):
+        return self.title
