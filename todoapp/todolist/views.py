0f8aef7b81fb3dbca52fa0d642851a0f13af1014
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-19 21:33:18 +0300

    гыгыггы

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,6 +1,7 @@
-
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+
 from .models import Todo
 
 
@@ -12,21 +13,22 @@ def index(request):
 
 
 @require_http_methods(['POST'])
+@csrf_exempt
 def add(request):
-    title = request.POST.get['title']
+    title = request.POST['title']
     todo = Todo(title=title)
     todo.save()
     return redirect('index')
-    
 
-def update(request,todo_id):
+
+def update(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.is_complete = not todo.is_complete
     todo.save()
     return redirect('index')
 
 
-def delete(request,todo_id):
+def delete(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.delete()
     return redirect('index')
