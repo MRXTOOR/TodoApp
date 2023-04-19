69128e1d363d73be165fcd93044cdc173d3fc282
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-19 19:35:50 +0300

    userapp

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,3 +1,11 @@
+
 from django.shortcuts import render
 
+from .models import Todo
+
+
 # Create your views here.
+
+def index(request):
+    todos = Todo.objects.all()
+    return render(request, 'todoapp/index.html', {'todolist':todos, 'title':'Главная страница'})
